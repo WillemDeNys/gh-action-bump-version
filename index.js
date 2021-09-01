7a8382a8dd10a343bd919aeee0e875c1582664b6
WillemDeNys <willem.de.nys@acerta.be>
2021-09-01 10:36:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ Toolkit.run(async (tools) => {
 
     const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
     if (process.env['INPUT_RESET-TAGS'] === 'true') {
-      await tools.runInWorkspace('git', ['tag', remoteRepo, '-d']);
+      await tools.runInWorkspace('git', ['tag -d']);
     }
     if (process.env['INPUT_SKIP-TAG'] !== 'true') {
       await tools.runInWorkspace('git', ['tag', newVersion]);
